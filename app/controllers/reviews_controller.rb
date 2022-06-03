936f7f06a4988da50319baf2b5879817958b0346
Marcus <marcusferreira23@gmail.com>
2022-06-03 10:56:37 -0700

    shows proper message for review creation.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,7 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
     if @review.save
-      flash[:notice] = "Product successfully added!"
+      flash[:notice] = "Review successfully added!"
       redirect_to product_path(@product)
     else
       render :new
