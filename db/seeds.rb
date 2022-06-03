e9d6f73a58f9d12deec44106ad55167ad2f135bb
Marcus <marcusferreira23@gmail.com>
2022-06-03 14:34:49 -0700

    add nested reviews in faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
+require 'faker'
+
 Product.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Games::Pokemon.name,
-                  cost: Faker::Number.number(digits: 2),
-                  country: Faker::WorldCup.team)
+  product = Product.create!(name: Faker::Games::Pokemon.name,
+                            cost: Faker::Number.number(digits: 2),
+                            country: Faker::WorldCup.team)
+  product.save()
+  5.times do |i|
+    product.reviews.create!(author: Faker::Games::SuperMario.character,
+                          content_body: Faker::Lorem.sentence(word_count: 25),
+                          rating: Faker::Number.within(range: 1..5))
   end
+end
+
 
 p "Created #{Product.count} products with #{Review.count} reviews"
