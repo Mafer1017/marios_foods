122eb33cc1c4f5cf795da7a8705a4a8e46db22a1
Marcus <marcusferreira23@gmail.com>
2022-06-02 23:12:33 -0700

    add titlization, test for it, and pass.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ApplicationRecord
   has_many :reviews
   validates :name, presence: true
   validates_length_of :name, maximum: 25
+
+  before_save(:titleize_product)
+
+ private
+   def titleize_product
+     self.name = self.name.titleize
+     self.country_of_origin = self.country_of_origin.titleize
+   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,9 @@ describe Product do
   it { should have_many(:reviews) }
   it { should validate_presence_of :name }
   it { should validate_length_of(:name).is_at_most(25) }
+  it("titleizes the name of a product and its country of origin") do
+    product = Product.create({name: "margherita pizza", cost: 15, country_of_origin: "italy"})
+    expect(product.name()).to(eq("Margherita Pizza"))
+    expect(product.country_of_origin()).to(eq("Italy"))
+  end
 end
