0ff1bfd6193e2cafbb8f3641cf25be323f1921ab
Marcus <marcusferreira23@gmail.com>
2022-06-18 22:07:03 -0700

    update tests to include authorization.

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 describe "the add a review process" do
-  before :each do
-    Product.create!({name: 'booger', cost: '15', country: 'United States'})
-    visit products_path
-    click_link 'Booger'
-    click_link 'Add a review'
+  before(:each) do
+    visit new_product_path
+    click_link 'Sign up'
+    fill_in 'user_email', with: 'user@aol.com'
+    fill_in 'user_password', with: 'dogdog'
+    fill_in 'user_password_confirmation', with: 'dogdog'
+    click_button 'Sign up'
   end
 
 
   it "adds a new review" do
+    product = Product.create(:name => 'Hamburger', :cost => '15', :country => 'United States')
+    visit product_path(product)
+    click_link 'Add a review'
     fill_in 'Author', :with => "Craig"
     fill_in 'Content body', :with => "This burger was the best burger I have ever eaten. It is amazing!"
     fill_in 'Rating', :with => 5
@@ -19,6 +24,9 @@ describe "the add a review process" do
   end
 
   it "returns an error when form is left blank" do
+    product = Product.create(:name => 'Hamburger', :cost => '15', :country => 'United States')
+    visit product_path(product)
+    click_link 'Add a review'
     click_on 'Create Review'
     expect(page).to have_content 'error'
   end
